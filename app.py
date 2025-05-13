793bf60c7cf895981a85120fd4c3fe186701fea1
alinawab1 <ali.nawab@rolustech.com>
2025-05-13 14:20:05 +0500

    The pdf file

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from pdf2image import convert_from_path
 import base64
 import os
 import tempfile
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/extract-text', methods=['POST'])
 def extract_text():
